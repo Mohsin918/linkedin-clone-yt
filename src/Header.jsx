127ff2d01ad62be26527d6d5f45c2da3c015ba4e
Ahmad <mohsin.ahmad@sap.com>
2023-07-24 21:55:42 +0530

    initial commit

---
+++ src/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='header'>
+
+        <div className='header__left'>
+
+        </div>
+
+        <div className='header__right'>
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default Header
