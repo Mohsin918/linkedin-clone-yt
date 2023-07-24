b0b6ed682537275914e5b3c228c09ab1f5a5aeac
Ahmad <mohsin.ahmad@sap.com>
2023-07-24 23:11:38 +0530

    created ugly header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
+import SearchIcon from '@mui/icons-material/Search';
+import './Header.css'
 
 function Header() {
   return (
     <div className='header'>
-
         <div className='header__left'>
+            <img 
+                src="https://cdn-icons-png.flaticon.com/512/174/174857.png" 
+                alt=""/>
+
+            <div className='header_search'>
+                <SearchIcon />
+                <input type='text'/>
+            </div>
 
         </div>
 
         <div className='header__right'>
 
         </div>
-      
     </div>
   )
 }
