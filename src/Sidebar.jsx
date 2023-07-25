e50cb35315babc4c0721949895fccf1bee05e2d7
Ahmad <mohsin.ahmad@sap.com>
2023-07-25 16:38:51 +0530

    finished sidebar

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -3,11 +3,22 @@ import './Sidebar.css'
 import { Avatar } from '@mui/material'
 
 function Sidebar() {
+
+    const recentItem = (topic) => (
+        <div className="sidebar__recentItem">
+            <span className="sidebar__hash">#</span>
+            <p>{topic}</p>
+        </div>
+    )
+
   return (
     <div className='sidebar'>
-        <div className="siderbar__top">
-            <img src='' alt=''/>
-            <Avatar className='sidebar__avatar'/>
+        <div className="sidebar__top">
+            <img 
+                src="https://images.unsplash.com/photo-1579546929518-9e396f3cc809?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHw%3D&w=1000&q=80"
+                alt=""
+            />
+            <Avatar className="sidebar__avatar"/>
             <h2>Mohsin Ahmad</h2>
             <h4>mohsinquantum@gmail.com</h4>
         </div>
@@ -15,17 +26,22 @@ function Sidebar() {
         <div className="sidebar__stats">
             <div className="sidebar__stat">
                 <p>Who viewed you:</p>
-                <div className="sidebar__statNumber">2,543</div>
+                <p className="sidebar__statNumber">2,543</p>
             </div>
 
             <div className="sidebar__stat">
                 <p>Views on post:</p>
-                <div className="sidebar__statNumber">2,543</div>
+                <p className="sidebar__statNumber">2,543</p>
             </div>
         </div>
 
         <div className="sidebar__bottom">
-
+            <p>Recent</p>
+            {recentItem('reactjs')}
+            {recentItem('programming')}
+            {recentItem('javaScript')}
+            {recentItem('softwareengineering')}
+            {recentItem('developer')}
         </div>
     </div>
   )
