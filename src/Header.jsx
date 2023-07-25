bdaa2c1a30655c31366f9c366d064c906afb9bed
Ahmad <mohsin.ahmad@sap.com>
2023-07-25 10:25:47 +0530

    finished header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import SearchIcon from '@mui/icons-material/Search';
+import HeaderOption from './HeaderOption';
+import HomeIcon from '@mui/icons-material/Home';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+
 import './Header.css'
 
 function Header() {
@@ -18,6 +25,13 @@ function Header() {
         </div>
 
         <div className='header__right'>
+          <HeaderOption Icon={HomeIcon} title='Home'/>
+          <HeaderOption Icon = {SupervisorAccountIcon} title='My Network'/>
+          <HeaderOption Icon = {BusinessCenterIcon} title='Jobs'/>
+          <HeaderOption Icon = {ChatIcon} title='Messaging'/>
+          <HeaderOption Icon = {NotificationsIcon} title='Notifications'/>
+          <HeaderOption avatar="https://compassionate-leakey-e9b16b.netlify.app/images/IG_Sonny.jpeg" title='me'/>
+
 
         </div>
     </div>
